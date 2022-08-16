4b1523f26ac4c6ebd873ec44264db050c41d10e6
Haoning Wu <whn@nousbuild.com>
2022-08-16 14:28:13 +0800

    🌈 Optimize remove bg code

--- 去除黑白底色底.py
+++ 去除黑白底色底.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os
 
 # 设定 png 图片目录，不要使用 C 盘路径，在 Windows 下 C 盘路径会受到保护。
-path = "D://Users//haoning//Downloads//480//"
+path = "D://yourpath//"
 files = os.listdir(path)
 
 for file in files:
@@ -15,7 +15,7 @@ for file in files:
 
     for item in datas:
         # 移除白色以及附近色彩
-        if item[0] > 246 and item[1] == 246 and item[2] == 246:
+        if item[0] > 246 and item[1] > 246 and item[2] > 246:
             newData.append((255, 255, 255, 0))
         # 移除黑色以及附近色彩
         elif item[0] < 64 and item[1] < 64 and item[2] < 64:
